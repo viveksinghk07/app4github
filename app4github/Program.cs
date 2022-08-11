8eabc65fc3b062325013c53c8832f2e8dc788bfa
viveksinghk07 <viveksinghk07@gmail.com>
2022-08-11 10:47:38 +0530

    new line added

--- app4github/Program.cs
+++ app4github/Program.cs
@@ -7,6 +7,7 @@ namespace app4github
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Today is Thursday");
         }
     }
 }
