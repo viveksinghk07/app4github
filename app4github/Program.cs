87c188360579579a7512367f5f5b473d3afb8bae
viveksinghk07 <viveksinghk07@gmail.com>
2022-08-11 09:32:13 +0530

    Add project files.

---
+++ app4github/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app4github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
